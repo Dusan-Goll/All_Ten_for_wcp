881a44cfe6b08e40c6a5c6be233a4d07db988779
Dusan-Goll <d.goll@seznam.cz>
2022-09-05 15:35:18 +0200

    I deleted 'generator.py' and 'trial_generator.py' because of their unusability

--- trial_generator.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-
-
-def generate():
-    text = ''
-    for i in range(15):
-        text = text + choice('fjdksl') + choice('fjghsl') + choice("dk';gh") \
-               + choice('dk":gh') + ' '
-    return text
